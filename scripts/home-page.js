010a8be59f0a83bd907b6b8f7feec4cde61a0979
Diego Gomez <diegogomez@MacBookAir.attlocal.net>
2025-09-02 20:22:57 -0400

    fix file paths for home-page.js

--- scripts/home-page.js
+++ scripts/home-page.js
@@ -1,9 +1,9 @@
 document.querySelector('.js-one-vs-one')
   .addEventListener('click', () => {
-    window.location.href = '/html/tic-tac-toe.html';
+    window.location.href = '../html/tic-tac-toe.html';
   });
 
 document.querySelector('.js-one-vs-computer')
   .addEventListener('click', () => {
-    window.location.href = '/html/computer-tic-tac-toe.html';
+    window.location.href = '../html/computer-tic-tac-toe.html';
   });
